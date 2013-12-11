1c3c2194deb38887240d4fd91194f68aec053a3c
brycedoig <brycedoig@gmail.com>
2013-12-11 11:33:58 -0800

    Ships are now triangles!

--- Game/Scripts/ship.lua
+++ Game/Scripts/ship.lua
@@ -1,19 +1,31 @@
-
 function MakeShip()
 	ship = {}
 
-	ship.x = 0
-	ship.y = 0
+	ship.velocity = 
+	{
+		x = 50, 
+		y = 50
+	}
+
+	ship.verts = 
+	{
+		x = {0, 10, 5}, 
+		y = {0, 0, 10}
+	}
 
 	ship.update = function(dt)
-		ship.x = ship.x + 100*dt
+		for k,v in pairs(ship.velocity) do			
+			for i = 1, 3 do
+				ship.verts[k][i] = ship.verts[k][i] + v * dt
+			end			
+		end
 	end
+
 	ship.draw = function()
 		love.graphics.setColor(255,255,255,255)
-		love.graphics.rectangle( "fill", ship.x-3, ship.y-3, 6, 6 )
+		love.graphics.polygon("fill", ship.verts.x[1], ship.verts.y[1], ship.verts.x[2], ship.verts.y[2],ship.verts.x[3], ship.verts.y[3] )
 	end
 
 	return ship
-
 end
 
