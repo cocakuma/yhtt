18a5417d2446ff0118d0c69818250d103eb86e0c
kleientertainment <johann.seidenz@kleientertainment.com>
2013-12-11 12:23:08 -0800

    Coroutines!

--- Game/Scripts/client/main.lua
+++ Game/Scripts/client/main.lua
@@ -1,15 +1,23 @@
-local socket=require ("socket")
-local settings = require("../../../settings")
-local conn = nil
+local coroutine = require('coroutine')
+local client = nil
 
-function love.load()
+function runclient()
+	local settings = require("../../../settings")
+	local socket=require ("socket")
 	print('Connecting to '..settings.server_ip..':'..settings.server_port)
-	conn = assert(socket.connect(settings.server_ip, settings.server_port))
+	local conn = assert(socket.connect(settings.server_ip, settings.server_port))
 	print('Connected!')	
+	while 1 do
+		coroutine.yield()
+	end
+end
+
+function love.load()	
+	client = coroutine.create(runclient)
 end
 
 function love.update(dt)
-
+	coroutine.resume(client)
 end
 
 function love.draw()
--- Game/Scripts/server/main.lua
+++ Game/Scripts/server/main.lua
@@ -1,23 +1,32 @@
-local socket=require ("socket")
-local settings = require("../../../settings")
-local conn = nil
+local coroutine = require('coroutine')
 local clients = {}
+local server = nil
 
-function love.load()
-	
-	conn = assert(socket.bind(settings.server_ip, settings.server_port))
+function runserver()
+	local settings = require('../../../settings')
+	local socket=require ('socket')
+	local conn = assert(socket.bind(settings.server_ip, settings.server_port))
 	local ip, port = conn:getsockname()
 	print('Listening for connections at '..ip..':'..port..'.')
+	conn:settimeout(0)
 	while 1 do
 		local client_conn = conn:accept()
-		local client_ip, client_port = client_conn:getpeername()
-		print('New connection '..client_ip..':'..client_port..'.')
-		table.insert(clients, client_conn)
+		if cleint_conn then
+			local client_ip, client_port = client_conn:getpeername()
+			print('New connection '..client_ip..':'..client_port..'.')
+			table.insert(clients, client_conn)
+		else
+			coroutine.yield()
+		end
 	end
 end
 
-function love.update(dt)
+function love.load()
+	server = coroutine.create(runserver)
+end
 
+function love.update(dt)
+	coroutine.resume(server)
 end
 
 function love.draw()
