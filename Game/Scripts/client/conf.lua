70a9862f8a087ce422e8773fc7f7308d0d9d73ff
kleientertainment <johann.seidenz@kleientertainment.com>
2013-12-13 10:10:40 -0800

    Added antialiasing.

--- Game/Scripts/client/conf.lua
+++ Game/Scripts/client/conf.lua
@@ -3,6 +3,7 @@ function love.conf(t)
 	t.title = "TRIANGLES WILL KILL YOU"
 	t.screen.width = 1280
 	t.screen.height = 720
+	t.screen.fsaa = 3
 
 	t.modules.joystick = false
 	t.modules.physics = false
