4afbb8654f83025075f42ce9a05e98b6bd758156
kleientertainment <johann.seidenz@kleientertainment.com>
2013-12-12 17:43:46 -0800

    More performance info.

--- Game/Scripts/client/main.lua
+++ Game/Scripts/client/main.lua
@@ -9,20 +9,27 @@ function love.update(dt)
 	client_update(dt)
 end
 
+local gDt = 0
+local tick_time = 1 / 30
 local last_tick = socket.gettime()
-function love.draw()
-	local tick_time = 1 / 30
+function love.draw()	
 	while socket.gettime() - last_tick < tick_time do	
 		updateclient(gClient)		
 	end
-	last_tick = socket.gettime()
+	local this_tick = socket.gettime()
+	gDt = this_tick - last_tick
+	last_tick = this_tick
 	client_draw()
 
 	local x = 1100
 	local y = 50
 	local y_delta = 15
+	love.graphics.print("Tick: "..round3(tick_time), x, y)
+	y = y + y_delta
+	love.graphics.print("DT: "..round3(gDt), x, y)
+	y = y + y_delta
 	love.graphics.print("Queued Frames: "..gQueuedFrames, x, y)
 	y = y + y_delta	
-	love.graphics.print("Render Dt: "..round3(gRenderDt), x, y)
+	love.graphics.print("Render: "..round3(gRenderDt), x, y)
 	y = y + y_delta	
 end
