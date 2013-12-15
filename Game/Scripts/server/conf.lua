be05b458105ab1376137e7a6be4d5572e80a9cc3
jseidenz <jseidenz@gmail.com>
2013-12-14 16:46:47 -0800

    Fix for unresponsive server.

--- Game/Scripts/server/conf.lua
+++ Game/Scripts/server/conf.lua
@@ -7,8 +7,8 @@ function love.conf(t)
 	t.modules.joystick = false
 	t.modules.physics = false
 	t.modules.audio = false
-	t.modules.event = false
+	--t.modules.event = false
 	t.modules.image = false
-	t.modules.timer = false
+	--t.modules.timer = false
 
 end
