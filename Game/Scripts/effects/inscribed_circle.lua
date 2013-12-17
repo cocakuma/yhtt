a4ea7ab84d5d6eb950c9db147869772b8322011e
kleientertainment <johann.seidenz@kleientertainment.com>
2013-12-17 00:01:50 -0800

    Better pixel shader.

--- Game/Scripts/effects/inscribed_circle.lua
+++
@@ -1,9 +0,0 @@
- return [[
- vec4 inset_color;
- vec4 outset_color;
- float inset_radius;
- vec4 effect(vec4 color, Image texture, vec2 texture_coords, vec2 pixel_coords)
- {
-    return color;
- }
- ]]
